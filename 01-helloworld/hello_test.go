a9fcf2efb7ffb76cdf7edc45a4bd2e1492b8b6b9
D3kion <dekion.dev@gmail.com>
2022-06-22 16:10:22 +0300

    Added some languages

--- 01-helloworld/hello_test.go
+++ 01-helloworld/hello_test.go
@@ -10,14 +10,24 @@ func TestHello(t *testing.T) {
 		}
 	}
 
+	t.Run("say \"Hello, World\" when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say \"Hello, World\" when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mario", "French")
+		want := "Bonjour, Mario"
 		assertCorrectMessage(t, got, want)
 	})
 }
