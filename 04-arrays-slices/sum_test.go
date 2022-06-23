c556a9be94fc9b987b56799eaadcf426891e62cd
D3kion <dekion.dev@gmail.com>
2022-06-23 15:10:48 +0300

    Added sum all feature

--- 04-arrays-slices/sum_test.go
+++ 04-arrays-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
@@ -10,5 +13,14 @@ func TestSum(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got '%d' want '%d', given %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
